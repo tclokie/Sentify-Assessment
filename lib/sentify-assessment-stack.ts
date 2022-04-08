94b6eece671f5c340b9127023db26fcd60271725
Trevor Clokie <tclokie@gmail.com>
2022-04-08 14:46:48 +1200

    Create an S3 bucket

--- lib/sentify-assessment-stack.ts
+++ lib/sentify-assessment-stack.ts
@@ -1,16 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_s3 } from 'aws-cdk-lib';
 
 export class SentifyAssessmentStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SentifyAssessmentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new aws_s3.Bucket(this, 'myBucket', {
+      versioned: true
+    });
   }
 }
